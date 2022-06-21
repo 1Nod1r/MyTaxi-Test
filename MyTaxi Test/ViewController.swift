60e8e7b280a736ab57759be7f6f4430b233bceb2
1Nod1r <kh.nodirbeek@gmail.com>
2022-06-21 20:48:28 +0500

    Initial Commit

---
+++ MyTaxi Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTaxi Test
+//
+//  Created by Nodirbek on 21/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
